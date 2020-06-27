854f6b45742494a0a80572796bb7831aac1fd061
dcca <dcca12@gmail.com>
2020-06-27 11:15:09 -0300

    add base for the check-in model

--- backend/src/models/checkIn.ts
+++ backend/src/models/checkIn.ts
@@ -1,1 +1,7 @@
-import {} from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
+
+interface ICheckIn extends Document {}
+
+const checkInSchema = new Schema({});
+
+export default model<ICheckIn>('CheckIn', checkInSchema);
