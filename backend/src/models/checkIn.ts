230257cfc2edd09b525f4b3d1b730605c67d7f78
dcca <dcca12@gmail.com>
2020-06-27 22:03:56 -0300

    add checkin model

--- backend/src/models/checkIn.ts
+++ backend/src/models/checkIn.ts
@@ -1,7 +1,35 @@
 import { Schema, model, Document } from 'mongoose';
 
-interface ICheckIn extends Document {}
+interface ICheckIn extends Document {
+  userId: string;
+  entry: Date;
+  exit: Date;
+  timeSpent: Date;
+}
 
-const checkInSchema = new Schema({});
+const checkInSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    entry: {
+      type: Date,
+      required: true,
+    },
+    exit: {
+      type: Date,
+    },
+    timeSpent: {
+      type: Date,
+      // TODO - Define a way to auto update this field with the input of the other fields
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+    },
+  }
+);
 
 export default model<ICheckIn>('CheckIn', checkInSchema);
